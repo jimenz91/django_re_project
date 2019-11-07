cb4bfb9d679851bb240d7a3098af315843ed3cf3
Carlos Jiménez <jimenz91@gmail.com>
2019-11-07 16:36:23 +0100

    Added a restriction so that registered users can't submit multiple inquiries to the same listing. Implemented email delivery for each inquiry to the corresponding realtor. Commented the feature and settings in order to not generate spam.

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# # Email configuration in case I want to send emails.
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.core.mail import send_mail
 from django.contrib import messages, auth
 
 from .models import Contact
@@ -7,6 +7,7 @@ from .models import Contact
 
 def contact(request):
     """Logic for retrieving information from contact form in single listing page"""
+    # Check if the method is a POST and obtaining all the fields from the inquiry.
     if request.method == 'POST':
         listing_id = request.POST['listing_id']
         listing = request.POST['listing']
@@ -17,12 +18,31 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
-    contact = Contact(listing=listing, listing_id=listing_id, name=name,
-                      email=email, phone=phone, message=message, user_id=user_id)
+        # Checking if the registered user has already submitted an inquirt for the listing
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id)
+            if has_contacted:
+                messages.error(
+                    request, 'You have already submitted an inquiry for this listing, a realtor will contact you soon.')
+            return redirect('/listings/'+listing_id)
+        # Creating the contact inquiry in the DB.
+        contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                          email=email, phone=phone, message=message, user_id=user_id)
 
-    contact.save()
+        contact.save()
 
-    messages.success(
-        request, 'Your request has been submitted, a realtor will get back to you soon')
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing +
+        #     '. Sign into the admin panel for more',
+        #     'email@gmail.com',
+        #     [realtor_email,],
+        #     fail_silently=False
+        # )
+
+        # Informing the user of the correct submission.
+        messages.success(
+            request, 'Your request has been submitted, a realtor will get back to you soon')
 
     return redirect('/listings/'+listing_id)
