7da12b60cb20af11d6e375469f0b98ba5ca65986
Carlos Jiménez <jimenz91@gmail.com>
2019-11-07 17:45:51 +0100

    Implemented the functionality for the user dashboard.

--- contacts/views.py
+++ contacts/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.core.mail import send_mail
-from django.contrib import messages, auth
-
 from .models import Contact
 
 
 def contact(request):
-    """Logic for retrieving information from contact form in single listing page"""
-    # Check if the method is a POST and obtaining all the fields from the inquiry.
+    """Logic for submitting inquiries in the single listing page."""
+    # Checking the type of method
     if request.method == 'POST':
         listing_id = request.POST['listing_id']
         listing = request.POST['listing']
@@ -18,31 +17,30 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
-        # Checking if the registered user has already submitted an inquirt for the listing
+        #  Check if user has made inquiry already
         if request.user.is_authenticated:
             user_id = request.user.id
-            has_contacted = Contact.objects.all().filter(listing_id=listing_id)
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
             if has_contacted:
                 messages.error(
-                    request, 'You have already submitted an inquiry for this listing, a realtor will contact you soon.')
-            return redirect('/listings/'+listing_id)
-        # Creating the contact inquiry in the DB.
+                    request, 'You have already made an inquiry for this listing')
+                return redirect('/listings/'+listing_id)
+
         contact = Contact(listing=listing, listing_id=listing_id, name=name,
                           email=email, phone=phone, message=message, user_id=user_id)
 
         contact.save()
 
+        # Send email
         # send_mail(
-        #     'Property Listing Inquiry',
-        #     'There has been an inquiry for ' + listing +
-        #     '. Sign into the admin panel for more',
-        #     'email@gmail.com',
-        #     [realtor_email,],
-        #     fail_silently=False
+        #   'Property Listing Inquiry',
+        #   'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+        #   'traversy.brad@gmail.com',
+        #   [realtor_email, 'techguyinfo@gmail.com'],
+        #   fail_silently=False
         # )
 
-        # Informing the user of the correct submission.
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon')
-
-    return redirect('/listings/'+listing_id)
+        return redirect('/listings/'+listing_id)
